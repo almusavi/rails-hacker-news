6e5ea61e61a117639ba80bf92159d9b3401a24b5
Al Musavi & JD Hiudt <pair+almusavi.jd-hiudt@devbootcamp.com>
2017-03-06 17:37:39 -0600

    reg and login function

--- hacker-news/config/routes.rb
+++ hacker-news/config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get '/' => 'posts#index'
+  #user
+  get '/users/new' => 'users#new', as: 'new_user'
+  post '/users' => 'users#create'
+
+  #sessions
+  get '/sessions/new' => 'sessions#new', as: 'new_session'
+  post '/sessions' => 'sessions#create'
+
+  #posts
+
+  #comments
 end
